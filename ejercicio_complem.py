4c4a58aee13dc607311010134d4e452fd8b6e1ec
Maximo <maxife924@gmail.com>
2023-08-16 17:03:32 -0300

    TP Ejercicio complementarios Máximo Fernandez

---
+++ ejercicio_complem.py
@@ -0,0 +1,62 @@
+#Ejercicio 1
+numero1 = 7 
+
+#Ejercicio 2 
+numero2 = 2.5
+
+#Ejercicio 3
+suma = numero1 + numero2
+
+#Ejercicio 4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2 
+
+print(f"SUMA: {suma}, RESTA: {resta}, MULTIPLICACION: {multiplicacion} ,DIVISION: {division}")
+
+#Ejercicio 5
+nombre = "Pedro"
+
+#Ejercicio 6 
+precio = 250.75
+
+#Ejercicio 7
+descuento = 0.25
+
+#Ejercicio 8 
+
+precio_final = precio - (precio * descuento)
+
+#Ejercicio 9 
+cadena = "Boca"
+
+#Ejercicio 10
+longitud = len(cadena)
+
+#Ejercicio 11
+precio = int(450.28)
+
+#Ejercicio 12
+nombre = "Maximo "
+apellido = "Fernandez"
+
+nombre_completo = nombre + apellido
+
+#Ejercicio 13
+edad = 19
+edad += 5
+edad -= 10
+
+#Ejercicio 14
+altura = 1.79
+altura *= 4
+altura /= 3
+
+print(altura)
+
+#Ejercicio 15
+nombre_mayus = "MAXIMO"
+nombre_minus = nombre_mayus.lower()
+
+#Ejercicio 16
+
