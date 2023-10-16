0a12420d061cd4e48ba907fe1cc0a73e6a78d983
Maximo <maxife924@gmail.com>
2023-10-16 18:15:54 -0300

    variables dimensionadas

--- funciones/variables_dimensionadas/ejercicio_variables dimensionadas.py
+++ funciones/variables_dimensionadas/ejercicio_variables dimensionadas.py
@@ -1,26 +1,66 @@
 import funciones_vd
-#Ejercicio 1
-datos_viajantes = [('Pedro Martinez',45532785,'Manchester'),('Gonzalo Ramos', 48563217, 'Lisboa'), ('Roger Martinez', 47553246, 'Chubut')]
-datos_pais = [('Manchester','Inglaterra'), ('Lisboa','Portugal'),('Chubut','Argentina')]
+##Ejercicio 1
+# datos_viajantes = [('Pedro Martinez',45532785,'Manchester'),('Gonzalo Ramos', 48563217, 'Lisboa'), ('Roger Martinez', 47553246, 'Chubut')]
+# datos_pais = [('Manchester','Inglaterra'), ('Lisboa','Portugal'),('Chubut','Argentina')]
 
-opcion = input("Que desea realizar:\n 1)Agregar pasajeros a la lista de viajeros.\n 2)Agregar ciudades a la lista de ciudades.\n 3)Dado el DNI de un pasajero saber a que ciudad viaja.\n 4)Dada una ciudad, mostar cuantos pasajeros viajan a esa ciudad.\n 5)Dado el DNI de un pasajero, ver a qué país viaja.\n 6)Dado un país, mostrar cuántos pasajeros viajan a ese país. \n 7)Salir.\n")
+# opcion = input("Que desea realizar:\n 1)Agregar pasajeros a la lista de viajeros.\n 2)Agregar ciudades a la lista de ciudades.\n 3)Dado el DNI de un pasajero saber a que ciudad viaja.\n 4)Dada una ciudad, mostar cuantos pasajeros viajan a esa ciudad.\n 5)Dado el DNI de un pasajero, ver a qué país viaja.\n 6)Dado un país, mostrar cuántos pasajeros viajan a ese país. \n 7)Salir.\n")
 
 
-if(opcion == "1"):
-    datos_viajantes.append(funciones_vd.agregar_pasajero())
-    print(datos_viajantes)
-elif(opcion == "2"):
-    datos_pais.append(funciones_vd.agregar_ciudad())
-    print(datos_pais)
-elif(opcion == "3"):
-    funciones_vd.buscar_destino_por_dni(datos_viajantes)
-elif(opcion == "4"):
-    funciones_vd.contar_pasajeros_ciudad(datos_viajantes)
-elif(opcion == "5"):
-    funciones_vd.buscar_pais_dni(datos_viajantes,datos_pais)
-elif(opcion == "6"):
-    funciones_vd.contar_pasajeros_pais(datos_pais)
-elif(opcion == "7"):
-    print("Hasta luego!")
+# if(opcion == "1"):
+#     datos_viajantes.append(funciones_vd.agregar_pasajero())
+#     print(datos_viajantes)
+# elif(opcion == "2"):
+#     datos_pais.append(funciones_vd.agregar_ciudad())
+#     print(datos_pais)
+# elif(opcion == "3"):
+#     funciones_vd.buscar_destino_por_dni(datos_viajantes)
+# elif(opcion == "4"):
+#     funciones_vd.contar_pasajeros_ciudad(datos_viajantes)
+# elif(opcion == "5"):
+#     funciones_vd.buscar_pais_dni(datos_viajantes,datos_pais)
+# elif(opcion == "6"):
+#     funciones_vd.contar_pasajeros_pais(datos_pais)
+# elif(opcion == "7"):
+#     print("Hasta luego!")
 
 ##Ejercicio 2
+compras = [
+    ('Nuria Costa', 5, 1234.5, 'Calle 1 - 456'),
+    ('Jorge Russo', 7, 3999, 'Calle 2 - 741'),
+    ('Nuria Costa', 12, 800.0, 'Calle 1 - 456'),
+    ('Laura Perez', 15, 1500.75, 'Calle 3 - 123')
+]
+
+domicilios = funciones_vd.obtener_domicilios_factura(compras)
+print(domicilios)
+
+#Ejercicio 3
+
+while True:
+    print("\nOpciones:")
+    print("1. Informar cantidad de socios")
+    print("2. Registrar que un socio ha pagado todas las cuotas adeudadas")
+    print("3. Modificar la fecha de ingreso de socios")
+    print("4. Dar de baja a un socio")
+    print("5. Imprimir listado de socios")
+    print("6. Salir")
+    
+    opcion = input("Elija una opción: ")
+    
+    if (opcion == "1"):
+        funciones_vd.informar_cantidad_socios()
+    elif (opcion == "2"):
+        numero_socio = int(input("Ingrese el número de socio que ha pagado todas las cuotas adeudadas: "))
+        funciones_vd.pagar_cuotas(numero_socio)
+    elif (opcion == "3"):
+        funciones_vd.modificar_fecha_ingreso()
+    elif (opcion == "4"):
+        nombre_apellido = input("Ingrese el nombre y apellido del socio que desea dar de baja: ")
+        funciones_vd.dar_de_baja(nombre_apellido)
+    elif (opcion == "5"):
+        funciones_vd.imprimir_listado_socios()
+    elif (opcion == "6"):
+        print("Hasta luego!")
+        break
+    else:
+        print("Opción no válida. Intente de nuevo.")
--- funciones/variables_dimensionadas/funciones_vd.py
+++ funciones/variables_dimensionadas/funciones_vd.py
@@ -1,4 +1,4 @@
-# Función para agregar pasajeros a la lista de viajeros
+#Ejercicio 1 
 def agregar_pasajero():
     tupla_pasajeros = ()
     nombre = input("Ingrese el nombre del pasajero: ").title()
@@ -42,3 +42,48 @@ def contar_pasajeros_pais(ciudades):
     pais = input("Ingrese el nombre del país: ").title()
     count = sum(1 for _, destino in ciudades if destino == pais)
     print(f"La cantidad de pasajeros que viajan a {pais} es {count}.")
+
+#Ejercicio 2
+def obtener_domicilios_factura(compras):
+    domicilios_factura = set()
+    
+    for compra in compras:
+        cliente, _, _, domicilio = compra  # Ignoramos el día y el monto
+        domicilios_factura.add(domicilio)
+    
+    return list(domicilios_factura)
+
+#Ejercicio 3 
+socios = {
+    1: {'nombre': 'Amanda Núñez', 'fecha_ingreso': '17/03/2009', 'cuota_al_dia': True},
+    2: {'nombre': 'Bárbara Molina', 'fecha_ingreso': '17/03/2009', 'cuota_al_dia': True},
+    3: {'nombre': 'Lautaro Campos', 'fecha_ingreso': '17/03/2009', 'cuota_al_dia': True}
+}
+def informar_cantidad_socios():
+    print(f"El club tiene {len(socios)} socios.")
+
+def pagar_cuotas(socio_numero):
+    if socio_numero in socios:
+        socios[socio_numero]['cuota_al_dia'] = True
+        print(f"El socio n°{socio_numero} ha pagado todas las cuotas adeudadas.")
+    else:
+        print(f"El socio n°{socio_numero} no se encuentra en la lista de socios.")
+
+# Modificar la fecha de ingreso de todos los socios ingresados el 13/03/2018
+def modificar_fecha_ingreso():
+    for socio_numero, datos_socio in socios.items():
+        if datos_socio['fecha_ingreso'] == '13/03/2018':
+            datos_socio['fecha_ingreso'] = '14/03/2018'
+
+# Solicitar el nombre y apellido de un socio y darle de baja
+def dar_de_baja(nombre_apellido):
+    for socio_numero, datos_socio in socios.copy().items():
+        if f"{datos_socio['nombre']} {datos_socio['apellido']}" == nombre_apellido:
+            del socios[socio_numero]
+            print(f"El socio {nombre_apellido} ha sido dado de baja.")
+
+# Función para imprimir el listado de socios completo
+def imprimir_listado_socios():
+    print("Listado de Socios:")
+    for socio_numero, datos_socio in socios.items():
+        print(f"Socio n°{socio_numero}: {datos_socio['nombre']}- Fecha de Ingreso: {datos_socio['fecha_ingreso']} - Cuota al día: {'Sí' if datos_socio['cuota_al_dia'] else 'No'}")
