fef225fa734f95ba66b5a49445b5494df4054cc0
Maximo <maxife924@gmail.com>
2023-10-17 16:57:06 -0300

    ejercicio en clase

---
+++ funciones/o_y_b/ejercicioclaseOyD/ejercicio_clase.py
@@ -0,0 +1,12 @@
+import funciones_ej_c_oyd
+##Ejercicio 1
+num_list = [64, 34, 25, 12, 22, 11, 90]
+print(num_list)
+
+funciones_ej_c_oyd.bubble_sort(num_list)
+
+funciones_ej_c_oyd.selection_sort(num_list)
+
+funciones_ej_c_oyd.inser_sort(num_list)
+
+funciones_ej_c_oyd.merge_sort(num_list)
