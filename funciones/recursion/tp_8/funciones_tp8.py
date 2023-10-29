44d7dfd9ee6f248bc0183cdfcb3ad2c9e4ea112a
Maximo <maxife924@gmail.com>
2023-10-29 19:43:35 -0300

    TP 8

--- funciones/recursion/tp_8/funciones_tp8.py
+++ funciones/recursion/tp_8/funciones_tp8.py
@@ -14,3 +14,77 @@ def potencia_de_otro(base,exponent):
         return False
     
 ##Ejercicio 3
+def position(phrase,b,pos=0):
+    if pos == len(phrase):
+        return []  
+
+    if phrase.startswith(b, pos):
+        return [pos] + position(phrase, b, pos + 1)
+    else:
+        return position(phrase, b, pos + 1)
+    
+##Ejercicio 4
+def even(num):
+    if num == 0:
+        return True  
+    else:
+        return odd(num - 1)
+
+def odd(num):
+    if num == 0:
+        return False  
+    else:
+        return even(num - 1) 
+    
+##Ejercicio 5
+def max_list_num(array_num, i, max_num):
+    if((len(array_num)-1) >= i):
+        if(array_num[i] > max_num):
+            max_num = array_num[i]
+        return max_list_num(array_num, i+1, max_num)
+    else:
+        return max_num
+    
+##Ejercicio 6
+def repeat_num(array_num, num_repeats):
+    if not array_num:
+        return []
+    current_element = array_num[0]
+    repeated_elements = [current_element] * num_repeats
+    return repeated_elements + repeat_num(array_num[1:], num_repeats)
+
+##Ejercicio 7 
+def sum_recursiva(n,p):
+    if (n>1):
+        return (p*n) + sum_recursiva(n-1,p)
+    elif(n <=0):
+        return ("n no puede valer ni ser menor a 0")
+    elif n == 1: 
+        return p
+    
+##Ejercicio 8
+def calculate_pascal(row, column):
+    if column == 0 or column==row:
+        return 1
+    else:
+        return calculate_pascal(row - 1, column - 1) + calculate_pascal(row - 1, column)
+    
+
+##Ejercicio 9 
+def combinations(char,n,built_chain=""):
+    if n==0:
+        print(built_chain)
+        return  
+    else:
+        for i in char:
+            combinations(char,n-1,built_chain+i)
+
+##Ejercicio 10
+def sheet_measurement(n):
+    if n==0:
+        return(841,1189)
+    previous_width, previous_length = sheet_measurement(n-1)
+    if n % 2 == 1:
+        return (previous_length, previous_width // 2)
+    else:
+        return (previous_width // 2, previous_length)
--- funciones/recursion/tp_8/tp_8.py
+++ funciones/recursion/tp_8/tp_8.py
@@ -1,17 +1,73 @@
 import funciones_tp8
+import random
 
-##Ejercicio 1
-# num = input("Ingrese un número entero positivo: ")
+#Ejercicio 1
+num = input("Ingrese un número entero positivo: ")
 
-# funciones_tp8.cant_digitos(num)
+funciones_tp8.cant_digitos(num)
 
-##Ejercicio 2
-# base = int(input("Ingrese la base: "))
-# exponent = int(input("Ingrese el exponente: "))
+#Ejercicio 2
+base = int(input("Ingrese la base: "))
+exponent = int(input("Ingrese el exponente: "))
 
-# print(funciones_tp8.potencia_de_otro(base, exponent))
+print(funciones_tp8.potencia_de_otro(base, exponent))
 
-##Ejercicio 3
+#Ejercicio 3
+phrase = input("Ingresa una frase: ")
+word_in_phrase = input("Ingresa la palabra a buscar: ")
+print(f"Posiciones donde se encontro {word_in_phrase} en {phrase}: {funciones_tp8.position(phrase, word_in_phrase)}")
+
+#Ejercicio 4
+number = int(input("Ingrese un numero: "))
+if funciones_tp8.even(number):
+    print(f"{number} es un numero par")
+else:
+    print(f"{number} es un numero impar")
+
+#Ejercicio 5
+array_num = []
+
+for i in range(10):
+    num = int(random.random()*100)
+    array_num.append(num)
+
+print(f"La lista es: {array_num}")
+print(f"El número mas grande de la lista es: {funciones_tp8.max_list_num(array_num, 0, 0)}")
+
+#Ejercicio 6
+array_num = []
+cant_num = int(input("Ingrese la cantidad de elementos que va a tener el arreglo: "))
+repeat_num = int(input("Ingrese la cantidad de veces que desea que se repitan los números: "))
+
+for i in range(cant_num):
+    num = int(random.random()*5)
+    array_num.append(num)
+
+print(f"El array original: {array_num}")
+print(f"El array números repetidos: {funciones_tp8.repeat_num(array_num, repeat_num)}")
+
+#Ejercicio 7
+number_1 = int(input("Ingrese el primer número: "))
+number_2 = int(input("Ingrese el segundo número: "))
+print(funciones_tp8.sum_recursiva(number_1,number_2))
+
+#Ejercicio 8
+print("El valor es: ", funciones_tp8.calculate_pascal(7,3))
+
+##Ejercicio 9
+characters = ["&","?","#","%","!"]
+number = 3
+funciones_tp8.combinations(characters,number)
+
+#Ejercicio 10
+number=1
+while True:
+    number=int(input("Ingrese un numero mayor que 0: "))
+    if number>0:
+        break
+    
+width, length = funciones_tp8.sheet_measurement(number)
+print(f'Ancho de A{number}: {width} mm, Largo de A{number}: {length} mm')
 
 
 
